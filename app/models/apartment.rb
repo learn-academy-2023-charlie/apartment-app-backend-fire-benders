ac1b01811ccd97d2ea887a33ee771f7c55ed74e7
Aubzzz <aubrey.diehl28@gmail.com>
2023-06-14 14:01:19 -0700

    api validation complete

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,4 +1,13 @@
 class Apartment < ApplicationRecord
     belongs_to :user
     validates :street, presence: true
+    validates :unit, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :square_footage, presence: true
+    validates :price, presence: true
+    validates :bedrooms, presence: true
+    validates :bathrooms, presence: true
+    validates :pets, presence: true
+    validates :image, presence: true
 end
